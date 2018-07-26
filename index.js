a306476b381d86a46d956688b7967fb7e7da7bc0
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 16:35:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer="Larry";
+
+
+//Reassigns the variable!
+function upperCaseCustomerName(){
+  customerName=customerName.toUpperCase();
+}
+//Creates a global scoped variable that's available for all to see! Oh no!
+function setBestCustomer(){
+  bestCustomer="not bob";
+}
+
+//Straight-up overrides that stuff - cuz its freely available in global scope!
+function overwriteBestCustomer(){
+  bestCustomer="maybe bob";
+}
+
+
+//Shows why a const is a good idea!
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer="Paige";
+}
